770bcdaf4c0312f997d7600ebdb4b2473fc0c620
acarrioning <a.carrion.urjc@gmail.com>
2014-10-30 19:34:57 +0100

    me falta por terminar los specs

---
+++ 08-missiles/spec/PlayerMissileSpec.js
@@ -0,0 +1,68 @@
+/*
+
+  Requisitos: 
+
+  La nave del usuario disparará 2 misiles si está pulsada la tecla de
+  espacio y ha pasado el tiempo de recarga del arma.
+
+  El arma tendrá un tiempo de recarga de 0,25s, no pudiéndose enviar
+  dos nuevos misiles antes de que pasen 0,25s desde que se enviaron
+  los anteriores
+
+
+
+  Especificación:
+
+  - Hay que añadir a la variable sprites la especificación del sprite
+    missile
+
+  - Cada vez que el usuario presione la tecla de espacio se añadirán
+    misiles al tablero de juego en la posición en la que esté la nave
+    del usuario. En el código de la clase PlayerSip es donde tienen
+    que añadirse los misiles
+
+  - La clase PlayerMissile es la que implementa los misiles. Es
+    importante que la creación de los misiles sea poco costosa pues va
+    a haber muchos disparos, para lo cual se declararán los métodos de
+    la clase en el prototipo
+
+*/
+
+
+describe("Missiles testing", function(){
+
+
+    var canvas, ctx;
+
+    beforeEach(function(){
+	loadFixtures('index.html');
+
+	canvas = $('#game')[0];
+	expect(canvas).toExist();
+
+	ctx = canvas.getContext('2d');
+	expect(ctx).toBeDefined();
+	
+	oldGame = Game;
+    });
+
+    afterEach(function(){
+	Game = oldGame;
+    }); 
+
+
+    it("Creamos missiles", function(){
+
+
+    });
+
+    it("Misil se eliminina al salir del canvas", function(){
+
+
+    });
+
+    it("hay que soltar la tecla de disparo", function(){
+
+
+    });
+});
